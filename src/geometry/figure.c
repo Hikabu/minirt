5d111a529d7644f188f69aa11444f4ca395209e4
armler <gevorgyanamin@gmail.com>
2024-02-16 17:22:25 +0400

    merged?

--- src/geometry/figure.c
+++ src/geometry/figure.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   figure.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: valeriafedorova <valeriafedorova@studen    +#+  +:+       +#+        */
+/*   By: armgevor <armgevor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 15:40:07 by valeriafedo       #+#    #+#             */
-/*   Updated: 2024/02/01 15:46:07 by valeriafedo      ###   ########.fr       */
+/*   Updated: 2024/02/16 17:22:08 by armgevor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ t_sphere	*init_sphere(t_vector *center, float radius)
     sphere = malloc(sizeof(t_sphere));
     if (!sphere)
         error(1);
+	printf("merged?");
     sphere->center = center;
     sphere->radius = radius;
     return(sphere);
